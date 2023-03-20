995ae2eb1e1c8a37e05e0aac0041d281e9f66f8a
Baby Leezus <118462761+babyleezus@users.noreply.github.com>
2023-03-20 18:43:45 +0000

    added if else statement for colour blocking

--- starter/script.js
+++ starter/script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
    
     
     $('.saveBtn').on('click', function() {
-
+        localStorage.setItem()
 
 
     })
@@ -18,6 +18,28 @@ $(document).ready(function() {
   //console.log(today);
 
   $('#currentDay').text(today.format('dddd Do MMMM YYYY'));
+
+  var currentHour = moment().hour();
+
+  let text = $('textarea').val(); //does this get the value inputted into all the 
+
+
+
+  $.each('.row', function()){
+    var timeblock = parseInt($(this).siblings(".hour").text()); //dont understand what this is supposed to do 
+  if (timeblock < currentHour) { 
+        $(this).addClass("past"); 
+    } else if (timeblock === currentHour) {
+        $(this).addClass("present");
+    } else {
+        $(this).addClass("future"); 
+    }
+  }
+
+  $('')
+
+
+  //console.log(currentHour);
 
    
 
